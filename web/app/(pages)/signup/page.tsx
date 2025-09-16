a755bef35542630db5c7eb04079865f13c916a82
semitha-dev <villaddarage@gmail.com>
2025-09-16 15:12:16 +0530

    Test 2

--- web/app/(pages)/signup/page.tsx
+++ web/app/(pages)/signup/page.tsx
@@ -1,54 +1,29 @@
-"use client"
-
 import React, { useState } from "react";
 
+  type FormState = {
+    name: string;
+    email: string;
+    password: string;
+    education_level: string;
+
+  }
 
 
-export default function SignupPage(){
+export default function signup(){
 
-    const [form, setForm] = useState({
-        name:'',
-        email:'',
-        password:'',
-        educationLevel:'',
+ const [form, setForm] = useState<FormState>({
+    name:'',
+    email:'',
+    password:'',
+    education_level:''
 
-    })
-    const [loading, setLoading] = useState('')
-    const [message, setMessage] = useState<string | null>(null)
+ })
 
+ const [loading, setLoading] = useState(false);
+ const [message, setMessage] = useState<string | null>(null);
 
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) =>{
-
-        setForm({...form, [e.target.name]: e.target.value})
-
-    }
-
-    const handleSignup = async (e: React.FormEvent) =>{
-
-    }
+ 
 
 
-    return(
-        <div className="h-screen w-full bg-[url('/sky.png')] bg-cover bg-center ">
-            <form
-            onSubmit={handleSignup}
-            className="bg-white "
-            >
-                <h1>Create an Account</h1>
 
-                <input
-                type="text"
-                name="name"
-                placeholder="eg: john doe"
-                value={form.name}
-                onChange={handleChange}
-                className="rounded border w-full m-1 "
-                />
-
-            </form>
-
-
-        </div>
-    )
 }
--- web/app/lib/supabaseClient.ts
+++ web/app/lib/supabaseClient.ts
@@ -1,6 +1,29 @@
-import { createClient } from '@supabase/supabase-js'
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+export async function createClient() {
+  const cookieStore = await cookies()
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  )
+}
