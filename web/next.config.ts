7f5a32ebe025dee53939a7d80c4dcbccc7f3bc50
semitha-dev <villaddarage@gmail.com>
2025-11-09 19:36:54 +0530

    w

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-      ignoreDuringBuilds: true, // ✅ allows build to succeed even if ESLint finds errors
+      eslint: {
+    ignoreDuringBuilds: true,
+  },
 
 };
 
