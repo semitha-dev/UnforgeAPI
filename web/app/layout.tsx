6e2e86d1abbcc2beaf976a925783fae990921427
semitha-dev <villaddarage@gmail.com>
2025-12-22 09:48:07 +0530

    Add Vercel Analytics

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import FeedbackButton from "./components/FeedbackButton";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
       >
         {children}
         <FeedbackButton />
+        <Analytics />
       </body>
     </html>
   );
